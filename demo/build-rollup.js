ae32c061c4a3f68f5b2ba35b0b0ab3d02274e615
gavan <guoming3@xiaomi.com>
2021-07-17 14:57:40 +0800

    misc.

--- demo/build-rollup.js
+++ demo/build-rollup.js
@@ -1,5 +1,5 @@
 const rollup = require('../dist/rollup')
 
-rollup(__dirname + '../src/rollup.js').then((res) => {
+rollup(__dirname + '/../src/rollup.js').then((res) => {
   res.wirte('build/rollup.js')
 })
